43d621fba4cb69e6ba087c9afe6af950568218dc
milomacphail <milomacphail@gmail.com>
2019-04-04 15:14:07 -0400

    Time of Day program

---
+++ TimeOfDay/TimeOfDay/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TimeOfDay
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour > 12 && hour < 18)
+            {
+                Console.WriteLine("Its afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("Its evening.");
+            }
+        }
+    }
+}
