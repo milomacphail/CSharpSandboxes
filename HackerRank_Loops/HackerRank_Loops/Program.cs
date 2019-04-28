87cf3d0d5892e49f046256b6ae7bf1f014991593
milomacphail <milomacphail@gmail.com>
2019-04-28 16:53:00 -0400

    Hacker Rank Loops

---
+++ HackerRank_Loops/HackerRank_Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int multiplier = 1; multiplier <= 10; multiplier++)
+        {
+            Console.WriteLine(n + " x " + multiplier + " = " + (multiplier * n));
+        }
+    }
+}
