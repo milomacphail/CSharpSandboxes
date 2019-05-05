5164d2460e6b4b44d87d85d7f2390f5bf9fa0c62
milomacphail <milomacphail@gmail.com>
2019-05-05 16:26:10 -0400

    Four Guesses

---
+++ FourGuesses/FourGuesses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FourGuesses
+{
+    class MainClass
+    {
+        public static void Main()
+        { 
+            int number = new Random().Next(1, 10);
+            Console.WriteLine(number);
+
+            for (int guesses = 0; guesses < 4; guesses++)
+            {
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == number)
+                {
+                    Console.WriteLine("Great job! You guessed the number!");
+                    return;
+                } 
+            }
+            Console.WriteLine("Oops! You lost!");
+        }
+    }
+}
